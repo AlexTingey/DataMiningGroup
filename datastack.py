7604eef055895fd153c9b1dedc2cd1b20cd85ee6
Alex <schmoo592@gmail.com>
2021-04-30 18:03:57 -0600

    curr version

--- datastack.py
+++ datastack.py
@@ -13,6 +13,7 @@ seen_dates = set()
 date_tuples = []
 last_date = None
 current_start_date_in_tuple = None
+start_date_BTC_yahoo_data = datetime(2014, 9, 19)
 for idx in range(len(file_list)):
     path = file_list[idx]
     try:
@@ -22,41 +23,45 @@ for idx in range(len(file_list)):
         for dataIdx in range(numRows):
             date = dataGen[dataIdx][0]
 
-            if date in seen_dates:
-                print(f"skipping date {date} because we have seen it before")
+            real_curr_date = datetime.utcfromtimestamp(date)
+
+            if (real_curr_date - start_date_BTC_yahoo_data).days < 0:
+                continue
             else:
-                seen_dates.add(date)
-                appendData.append(dataGen[dataIdx])
-                if last_date == None and current_start_date_in_tuple == None:
-                    last_date = date
-                    current_start_date_in_tuple = date
-                elif last_date != None:
-                    real_curr_date = datetime.utcfromtimestamp(date)
-                    last_date_real = datetime.utcfromtimestamp(last_date)
-                    if abs((real_curr_date - last_date_real).days > 1):
-                        real_current_start_date = datetime.utcfromtimestamp(current_start_date_in_tuple)
-
-                        start_date_format = real_current_start_date.strftime('%Y-%m-%d')
-                        last_date_format = last_date_real.strftime('%Y-%m-%d')
-                        date_tuples.append((start_date_format, last_date_format))
-
+                if date in seen_dates:
+                    print(f"skipping date {date} because we have seen it before")
+                else:
+                    seen_dates.add(date)
+                    appendData.append(dataGen[dataIdx])
+                    if last_date == None and current_start_date_in_tuple == None:
+                        last_date = date
                         current_start_date_in_tuple = date
-                    last_date = date
+                    elif last_date != None:
+                        last_date_real = datetime.utcfromtimestamp(last_date)
+                        if abs((real_curr_date - last_date_real).days > 1):
+                            real_current_start_date = datetime.utcfromtimestamp(current_start_date_in_tuple)
+
+                            start_date_format = real_current_start_date.strftime('%Y-%m-%d')
+                            last_date_format = last_date_real.strftime('%Y-%m-%d')
+                            date_tuples.append((start_date_format, last_date_format))
+
+                            current_start_date_in_tuple = date
+                        last_date = date
         if(len(appendData) == 0):
             continue
         else:
             data.append(appendData)
     except Exception as e:
-        print(path + " Error! " + e)
+        print(path + " Error! " + str(e))
 real_current_start_date = datetime.utcfromtimestamp(current_start_date_in_tuple)
 
 start_date_format = real_current_start_date.strftime('%Y-%m-%d')
-last_date_format = last_date_real.strftime('%Y-%m-%d')
-date_tuples.append((start_date_format, last_date_format))
+curr_date_fmt = real_curr_date.strftime('%Y-%m-%d')
+date_tuples.append((start_date_format, curr_date_fmt))
 full_data = np.vstack(data)
 final_data = pd.DataFrame.from_records(full_data)
 
 print(final_data)
+print(date_tuples)
 
 final_data.to_csv('./dataFiles/stackedData.csv')
-print(date_tuples)
